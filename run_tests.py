ae87b53fe60a85b42dbc4063a26f62cacd3758f4
shelo <marjara35@gmail.com>
2016-04-25 21:24:06 -0300

    Update some tests

--- run_tests.py
+++ run_tests.py
@@ -7,13 +7,13 @@ if __name__ == '__main__':
     file_tests.test_engine.run("remove_force")
     print file_tests.test_engine.stats()
 
+    prompt_tests.test_engine.run("vim")
+    prompt_tests.test_engine.run("confirm")
+    print prompt_tests.test_engine.stats()
+
     system_tests.test_engine.run("cmd")
     print system_tests.test_engine.stats()
 
     text_tests.test_engine.run("replace")
     text_tests.test_engine.run("contains")
     print text_tests.test_engine.stats()
-
-    prompt_tests.test_engine.run("vim")
-    prompt_tests.test_engine.run("confirm")
-    print prompt_tests.test_engine.stats()
--- tests/file_tests.py
+++ tests/file_tests.py
@@ -3,15 +3,19 @@ import pyfs
 
 
 def test_copy():
-    pyfs.file.copy("testdir/file1.md", "testdir/file2.md")
+    pyfs.file.copy("testdir/file_copy.md", "testdir/file_copy_cp.md")
+
+    with open("testdir/file_copy.md") as f1:
+        with open("testdir/file_copy_cp.md") as f2:
+            assert f1.read() == f2.read()
 
 
 def test_remove():
-    pyfs.file.remove("testdir/file2.md")
+    pyfs.file.remove("testdir/file_copy_cp.md")
 
 
 def test_remove_force():
-    pyfs.file.remove("testdir/file2.md", ignore=True)
+    pyfs.file.remove("testdir/no_existent.md", ignore=True)
 
 
 test_engine = TestEngine(__name__)
